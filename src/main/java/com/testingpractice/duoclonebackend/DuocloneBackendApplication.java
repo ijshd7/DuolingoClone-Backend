80993e256dd9ca05b5d0b40ad87f5030e0f9dd41
Dave <davidglogowski2003@gmail.com>
2025-08-17 18:37:17 +0200

    First Commit

---
+++ src/main/java/com/testingpractice/duoclonebackend/DuocloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.testingpractice.duoclonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuocloneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuocloneBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testingpractice/duoclonebackend/DuocloneBackendApplicationTests.java
@@ -0,0 +1,22 @@
+package com.testingpractice.duoclonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuocloneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        
+
+    }
+
+}
+
+
+
+
+
+
