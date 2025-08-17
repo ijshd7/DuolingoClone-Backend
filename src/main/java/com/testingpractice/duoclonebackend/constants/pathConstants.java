7710d9f18d6a093ac31d5e366d9cff0d3d2f7941
Dave <davidglogowski2003@gmail.com>
2025-08-18 01:19:37 +0200

    Add path and units class test constants

---
+++ src/main/java/com/testingpractice/duoclonebackend/constants/pathConstants.java
@@ -0,0 +1,11 @@
+package com.testingpractice.duoclonebackend.constants;
+
+public class pathConstants {
+
+    public static final String API_V1 = "/api";
+
+    public static final String COURSES = API_V1 + "/courses";
+    public static final String COURSE_UNITS = COURSES + "/{courseId}/units";
+
+}
+
---
+++ src/test/java/com/testingpractice/duoclonebackend/testutils/TestConstants.java
@@ -0,0 +1,23 @@
+package com.testingpractice.duoclonebackend.testutils;
+
+public class TestConstants {
+
+    //----UNITS----
+    public static String UNIT_1_TITLE = "Discuss a new Job";
+    public static String UNIT_2_TITLE = "Talk about your habits";
+    public static String UNIT_3_TITLE = "Pack for a vacation";
+    public static String UNIT_4_TITLE = "Plan Dinner";
+    public static String UNIT_5_TITLE = "Use complex verbs";
+    public static String UNIT_6_TITLE = "Argue with a roommate";
+
+     //----COURSES----
+    public static Integer COURSE_1_ID = 1;
+    public static Integer COURSE_2_ID = 2;
+
+     //----COURSES----
+    public static Integer SECTION_1_ID = 1;
+    public static Integer SECTION_2_ID = 2;
+
+
+
+}
