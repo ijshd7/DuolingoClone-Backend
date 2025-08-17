e73c0586fc30b1c9c8e92b60a7cafa08d122c3c0
Dave <davidglogowski2003@gmail.com>
2025-08-17 22:44:48 +0200

    switch add UnitService interface and move old to UnitServiceImpl

---
+++ src/main/java/com/testingpractice/duoclonebackend/service/UnitServiceImpl.java
@@ -0,0 +1,30 @@
+package com.testingpractice.duoclonebackend.service;
+
+import com.testingpractice.duoclonebackend.dto.UnitDto;
+import com.testingpractice.duoclonebackend.entity.Unit;
+import com.testingpractice.duoclonebackend.mapper.UnitMapper;
+import com.testingpractice.duoclonebackend.repository.UnitRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UnitServiceImpl implements UnitService{
+
+    private final UnitRepository unitRepository;
+    private final UnitMapper unitMapper;
+
+    @Autowired
+    public UnitServiceImpl (UnitRepository unitRepository, UnitMapper unitMapper) {
+        this.unitRepository = unitRepository;
+        this.unitMapper = unitMapper;
+    }
+
+    @Override
+    public List<UnitDto> getUnitsByCourse (Integer courseId) {
+        List<Unit> units = unitRepository.findAllByCourseId(courseId);
+        return unitMapper.toDtoList(units);
+    }
+
+}
