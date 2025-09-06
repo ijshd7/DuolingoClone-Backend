b9b2ed3f1e21d8020a07825d5327415b46fd6e33
Dave <davidglogowski2003@gmail.com>
2025-09-06 17:01:04 +0200

    Misc: Add descriptions to path constants

--- src/main/java/com/testingpractice/duoclonebackend/constants/pathConstants.java
+++ src/main/java/com/testingpractice/duoclonebackend/constants/pathConstants.java
@@ -2,30 +2,40 @@ package com.testingpractice.duoclonebackend.constants;
 
 public class pathConstants {
 
-    public static final String API_V1 = "/api";
+  public static final String API_V1 = "/api";
 
-    public static final String COURSES = API_V1 + "/courses";
+  // ----------------------------- API PREFIXES -----------------------------
+  public static final String COURSES = API_V1 + "/courses";
+  public static final String EXERCISES = API_V1 + "/exercises";
+  public static final String UNITS = API_V1 + "/units";
+  public static final String USERS = API_V1 + "/users";
+  public static final String LESSONS = API_V1 + "/lessons";
+  public static final String SECTIONS = API_V1 + "/sections";
 
-    public static final String EXERCISES = API_V1 + "/exercises";
-    public static final String SUBMIT_EXERCSIE = "/submit";
+  // ----------------------------- GET REQUESTS -----------------------------
+  public static final String GET_USER_COURSE_PROGRESS = "/progress/{courseId}/{userId}";
 
-    public static final String USERS = API_V1 + "/users";
-    public static final String GET_USER_COURSE_PROGRESS = "/progress/{courseId}/{userId}";
+  // ===== GET DTO LIST BY PARENT =====
+  public static final String GET_UNITS_BY_SECTION = "/{sectionId}/units";
+  public static final String GET_LESSONS_BY_UNIT = "/{unitId}/{userId}/lessons";
+  public static final String GET_SECTIONS_BY_COURSE = "/{courseID}/sections";
 
-    public static final String UNITS = API_V1 + "/units";
-    public static final String UNIT_LESSONS = "/{unitId}/{userId}/lessons";
-    public static final String LESSONS_UNITS_IDS = UNIT_LESSONS + "/ids";
-    public static final String UNITS_FROM_IDS = "/ids";
+  //!! TRANSACTIONAL !!//
+  public static final String GET_EXERCISES_BY_LESSON = "{lessonId}/{userId}/exercises";
+
+  // ===== GET DTO LIST FROM IDS =====
+  public static final String GET_UNITS_FROM_IDS = "/ids";
+  public static final String GET_LESSONS_FROM_IDS = "/ids";
+  public static final String GET_SECTIONS_FROM_IDS = "/ids";
+
+  // ===== GET IDS LIST BY PARENT =====
+  public static final String GET_LESSON_IDS_BY_UNIT = GET_LESSONS_BY_UNIT + "/ids";
+  public static final String GET_UNIT_IDS_BY_SECTION = GET_UNITS_BY_SECTION + "/ids";
+  public static final String GET_SECTION_IDS_BY_COURSE = GET_SECTIONS_BY_COURSE + "/ids";
+
+  // ----------------------------- POST REQUESTS AND MUTATIONS -----------------------------
+  public static final String SUBMIT_EXERCSIE = "/submit";
+  public static final String SUBMIT_COMPLETED_LESSON = "/completedLesson";
 
 
-    public static final String LESSONS = API_V1 + "/lessons";
-    public static final String LESSONS_FROM_IDS = "/ids";
-    public static final String LESSON_EXERCISES = "{lessonId}/{userId}/exercises";
-
-    public static final String SECTIONS = API_V1 + "/sections";
-    public static final String SECTIONS_FROM_IDS = "/ids";
-    public static final String SECTION_UNITS = "/{sectionId}/units";
-    public static final String SECTION_UNITS_IDS = SECTION_UNITS + "/ids";
-    public static final String SECTIONS_FROM_COURSE_ID = SECTION_UNITS + "/ids";
 }
-
