3636221b36712b249d1f61935f5fbf17e80087da
Dave <davidglogowski2003@gmail.com>
2025-09-24 21:36:46 +0200

    Add quests controller and get quests by user id method

--- src/main/java/com/testingpractice/duoclonebackend/domain/controller/QuestController.java
+++ src/main/java/com/testingpractice/duoclonebackend/domain/controller/QuestController.java
@@ -1,3 +1,30 @@
 package com.testingpractice.duoclonebackend.domain.controller;
 
-public class QuestController {}
+import com.testingpractice.duoclonebackend.constants.pathConstants;
+import com.testingpractice.duoclonebackend.dto.QuestResponse;
+import com.testingpractice.duoclonebackend.service.QuestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(pathConstants.QUESTS)
+public class QuestController {
+
+    private final QuestService questService;
+
+    @GetMapping(pathConstants.GET_QUESTS_BY_USER)
+    public List<QuestResponse> getQuestsByUserId (
+            @PathVariable Integer userId
+    ) {
+        return questService.getQuestsForUser(userId);
+    }
+
+
+
+}
