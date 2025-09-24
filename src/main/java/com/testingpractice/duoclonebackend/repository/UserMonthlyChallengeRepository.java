183b3d766c8f6e4a18ef321029a510452550c191
Dave <davidglogowski2003@gmail.com>
2025-09-24 23:52:11 +0200

    Add monthly challenge repositories and UserMonthlyChallenge entity properties

---
+++ src/main/java/com/testingpractice/duoclonebackend/repository/UserMonthlyChallengeRepository.java
@@ -0,0 +1,10 @@
+package com.testingpractice.duoclonebackend.repository;
+
+import com.testingpractice.duoclonebackend.entity.UserMonthlyChallenge;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserMonthlyChallengeRepository extends JpaRepository<UserMonthlyChallenge, Integer> {
+
+
+
+}
