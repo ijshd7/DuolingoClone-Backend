83b1df5cdff16b77479d2a3c3487eddca2dfa3c6
Dave <davidglogowski2003@gmail.com>
2025-09-24 17:45:38 +0200

    Fix: correct path constants for submissions

--- src/main/java/com/testingpractice/duoclonebackend/constants/pathConstants.java
+++ src/main/java/com/testingpractice/duoclonebackend/constants/pathConstants.java
@@ -12,8 +12,8 @@ public class pathConstants {
   public static final String LESSONS = API_V1 + "/lessons";
   public static final String SECTIONS = API_V1 + "/sections";
 
-  public static final String EXERCISES_ATTEMPTS = API_V1 + EXERCISES + "/attempts";
-  public static final String LESSONS_COMPLETIONS = API_V1 + LESSONS + "/completions";
+  public static final String EXERCISES_ATTEMPTS = EXERCISES + "/attempts";
+  public static final String LESSONS_COMPLETIONS = LESSONS + "/completions";
 
   // ----------------------------- GET REQUESTS -----------------------------
   public static final String GET_USER_COURSE_PROGRESS = "/progress/{courseId}/{userId}";
