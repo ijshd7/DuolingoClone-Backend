7147d15f5d68085c4c909fe1de5f2e5db8bc7585
Dave <davidglogowski2003@gmail.com>
2025-09-25 00:11:43 +0200

    Make MonthlyChallengeController return monthly challenge for user

--- src/main/java/com/testingpractice/duoclonebackend/domain/controller/MonthlyChallengeController.java
+++ src/main/java/com/testingpractice/duoclonebackend/domain/controller/MonthlyChallengeController.java
@@ -3,6 +3,7 @@ package com.testingpractice.duoclonebackend.domain.controller;
 
 import com.testingpractice.duoclonebackend.constants.pathConstants;
 import com.testingpractice.duoclonebackend.dto.QuestResponse;
+import com.testingpractice.duoclonebackend.service.MonthlyChallengeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(pathConstants.MONTHLY_CHALLENGES)
 public class MonthlyChallengeController {
 
+    private final MonthlyChallengeService monthlyChallengeService;
+
     @GetMapping(pathConstants.GET_MONTHLY_CHALLENGE_BY_USER)
     public QuestResponse getMonthlyChallenge (@PathVariable Integer userId) {
-
+        return monthlyChallengeService.getMonthlyChallengeForUser(userId);
     }
 
 
