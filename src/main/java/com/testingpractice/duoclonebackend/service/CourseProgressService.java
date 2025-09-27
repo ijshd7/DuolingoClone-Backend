4acc2a7e230e8b1bd50a0f7c927887ed54dd2c2d
Dave <davidglogowski2003@gmail.com>
2025-09-27 21:48:01 +0200

    Add get lesson section id helper method

--- src/main/java/com/testingpractice/duoclonebackend/service/CourseProgressService.java
+++ src/main/java/com/testingpractice/duoclonebackend/service/CourseProgressService.java
@@ -1,9 +1,11 @@
 package com.testingpractice.duoclonebackend.service;
 
 import com.testingpractice.duoclonebackend.entity.Lesson;
+import com.testingpractice.duoclonebackend.entity.Unit;
 import com.testingpractice.duoclonebackend.entity.UserCourseProgress;
 import com.testingpractice.duoclonebackend.exception.ApiException;
 import com.testingpractice.duoclonebackend.exception.ErrorCode;
+import com.testingpractice.duoclonebackend.repository.LessonRepository;
 import com.testingpractice.duoclonebackend.repository.UserCourseProgressRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,8 @@ public class CourseProgressService {
 
     private final UserCourseProgressRepository userCourseProgressRepository;
     private final CurriculumNavigator curriculumNavigator;
+    private final LessonRepository lessonRepository;
+    private final LookupService lookupService;
 
     @Transactional
     public UserCourseProgress updateUsersNextLesson (Integer userId, Integer courseId, Lesson currentLesson) {
@@ -39,4 +43,13 @@ public class CourseProgressService {
         return userCourseProgress;
 
    }
+
+   public Integer getLessonSectionId (Integer lessonId) {
+        Lesson lesson = lookupService.lessonOrThrow(lessonId);
+        Integer unitId = lesson.getUnitId();
+       Unit unit = lookupService.unitOrThrow(unitId);
+       Integer sectionId = unit.getSectionId();
+       return sectionId;
+   }
+
 }
