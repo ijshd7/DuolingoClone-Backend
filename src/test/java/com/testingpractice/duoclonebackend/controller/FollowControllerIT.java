08c7cd81caf7143022f434748d781e1d98e82f2a
Dave <davidglogowski2003@gmail.com>
2025-10-02 16:28:33 +0200

    Add unfollow test to FollowControllerIT

--- src/test/java/com/testingpractice/duoclonebackend/controller/FollowControllerIT.java
+++ src/test/java/com/testingpractice/duoclonebackend/controller/FollowControllerIT.java
@@ -2,7 +2,6 @@ package com.testingpractice.duoclonebackend.controller;
 
 import com.testingpractice.duoclonebackend.constants.pathConstants;
 import com.testingpractice.duoclonebackend.dto.FollowResponse;
-import com.testingpractice.duoclonebackend.dto.LessonCompleteResponse;
 import com.testingpractice.duoclonebackend.entity.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +11,7 @@ import java.util.Map;
 
 import static com.testingpractice.duoclonebackend.testutils.TestConstants.FIXED_TIMESTAMP_1;
 import static com.testingpractice.duoclonebackend.testutils.TestConstants.FIXED_TIMESTAMP_2;
+import static com.testingpractice.duoclonebackend.testutils.TestUtils.makeFollow;
 import static com.testingpractice.duoclonebackend.testutils.TestUtils.makeUser;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -24,6 +24,9 @@ public class FollowControllerIT extends AbstractIntegrationTest{
 
     }
 
+    private String followPath = pathConstants.FOLLOWS + pathConstants.FOLLOW_USER;
+    private String unfollowPath = pathConstants.FOLLOWS + pathConstants.UNFOLLOW_USER;
+
     @Test
     void followUser_noFollowExists_createsFollow () {
 
@@ -33,7 +36,7 @@ public class FollowControllerIT extends AbstractIntegrationTest{
         User followed = userRepository.save(makeUser(course1.getId(), "testUser2", "test2", "user2", "testemail2", "pfp", 0, FIXED_TIMESTAMP_1, FIXED_TIMESTAMP_2, 0));
         Integer followedId = followed.getId();
 
-        FollowResponse response = submitLessonBody(followerId, followedId);
+        FollowResponse response = submitFollowBody(followerId, followedId, followPath);
 
         assertThat(response).isNotNull();
         assertThat(response.followedNewStats().followerIds().size()).isEqualTo(1);
@@ -43,7 +46,28 @@ public class FollowControllerIT extends AbstractIntegrationTest{
 
     }
 
-    private FollowResponse submitLessonBody (Integer userId, Integer followedId) {
+    @Test
+    void unfollowUser_followExists_unfollowsUser () {
+
+        User follower = userRepository.save(makeUser(course1.getId(), "testUser", "test", "user", "testemail", "pfp", 0, FIXED_TIMESTAMP_1, FIXED_TIMESTAMP_2, 0));
+        Integer followerId = follower.getId();
+
+        User followed = userRepository.save(makeUser(course1.getId(), "testUser2", "test2", "user2", "testemail2", "pfp", 0, FIXED_TIMESTAMP_1, FIXED_TIMESTAMP_2, 0));
+        Integer followedId = followed.getId();
+
+        followRepository.save(makeFollow(followerId, followedId, FIXED_TIMESTAMP_1));
+
+        FollowResponse response = submitFollowBody(followerId, followedId, unfollowPath);
+
+        assertThat(response).isNotNull();
+        assertThat(response.followedNewStats().followerIds().size()).isEqualTo(0);
+        assertThat(response.followedNewStats().followingIds().size()).isEqualTo(0);
+        assertThat(response.followersNewStats().followingIds().size()).isEqualTo(0);
+        assertThat(response.followersNewStats().followerIds().size()).isEqualTo(0);
+
+    }
+
+    private FollowResponse submitFollowBody(Integer userId, Integer followedId, String path) {
         return
                 given()
                         .header("X-Test-User-Id", userId)
@@ -54,7 +78,7 @@ public class FollowControllerIT extends AbstractIntegrationTest{
                                 )
                         )
                         .when()
-                        .post(pathConstants.FOLLOWS + pathConstants.FOLLOW_USER)
+                        .post(path)
                         .then()
                         .statusCode(200)
                         .extract()
